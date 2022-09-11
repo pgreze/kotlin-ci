7edc6b0e92609a7d3cd66ca2d0ff2785774c75a1
pgreze <pierrick.greze@gmail.com>
2022-09-11 17:33:36 +0900

    Create the kotlin-ci-android subproject

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ This was used in:
 + a DroidKaigi prensentation (TODO: link)
 + a Mercari blog-post (TODO: link)
 
+The project is including 2 separate projects:
++ the [./android](./android) project representing a consumer Android codebase.
++ the [./toolbox](./toolbox) project implementing Docker shipped utilities used in the consumer projects.
+
 ## Features
 
 + Custom docker images
---
+++ android/settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.40.2"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin-ci-android"
+
+include("app")
+include("core")
+include("domain")
+include("scripts")
+// Only load scripts folder if required.
+//val includeScripts: String? by settings
+//if ("true" in arrayOf(System.getenv("CI"), includeScripts)) {
+//    includeBuild("scripts")
+//}
